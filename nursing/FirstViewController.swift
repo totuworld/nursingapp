a95e37c8ef02d30b82599f29087cd96921e9174c
totuworld <totuworld@gmail.com>
2015-09-03 18:12:33 +0900

    Initial Commit

---
+++ nursing/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  nursing
+//
+//  Created by Yochang Song on 2015. 9. 3..
+//  Copyright (c) 2015년 Yochang Song. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
